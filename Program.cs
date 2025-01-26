8955db4305d4655a4ee0a2d40de3e6d185bc9b4a
Ricardo Cruz <Ricardo_Cruz_41@hotmail.com>
2025-01-26 23:42:16 +0000

    - revert changes to program

--- Program.cs
+++ Program.cs
@@ -2,23 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-app.UseCors("AllowFrontend");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(policy =>
+{
+    policy.AllowAnyOrigin() 
+          .AllowAnyHeader()
+          .AllowAnyMethod();
+});
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
