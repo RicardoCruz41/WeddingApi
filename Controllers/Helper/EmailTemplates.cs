97e2f665c8a6cc86fc657dc97dbaa5c9c20ef64d
Ricardo Cruz <Ricardo_Cruz_41@hotmail.com>
2025-05-03 19:27:37 +0100

    - change email template

--- Controllers/Helper/EmailTemplates.cs
+++ Controllers/Helper/EmailTemplates.cs
@@ -14,7 +14,7 @@
             return language == "en"
                 ? $"Dear {name},\n\nThank you very much for being present on such an important day in our lives. " +
                   "Please don't forget to mark this special date for us on your calendar, so you don't forget it.\n\n" +
-                  "To help you remember, our wedding will be on September 12, 2025, starting around 5 PM at the Vandelli Botanical Garden, " +
+                  "To help you remember, our wedding will be on September 12, 2025, starting around 4:30 PM at the Vandelli Botanical Garden, " +
                   "located at Calçada do Galvão E, 1400-171 Lisbon.\n https://www.google.com/maps/place/Vandelli+Botanical+Garden/@38.7067187,-9.2017967,17z/data=!3m1!4b1!4m6!3m5!1s0xd1ecb4279c1a8cd:0x800851f444044593!8m2!3d38.7067187!4d-9.2017967!16s%2Fg%2F11s9dk7_r6?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D \n\n" +
                   "If you would like to give us a wedding gift, we would like to remind you that you can do so through the following methods:\n\n" +
                   "MBWAY: 918960273 / 925355133\n" +
@@ -22,7 +22,7 @@
                   "We hope to see you at our wedding!\n\nHugs and kises,\n\nRicardo and Carolina"
                 : $"{name},\n\n Muito obrigado por estares presente neste dia tão importante nas nossas vidas. " +
                   "Não te esqueças de marcar esta data tão especial para nós no teu calendário, de forma a não te esqueceres da data.\n\n" +
-                  "Para relembrar, o nosso casamento será no dia 12 de Setembro de 2025, a começar perto das 17h no Vandelli Botanical Garden, " +
+                  "Para relembrar, o nosso casamento será no dia 12 de Setembro de 2025, a começar perto das 16:30h no Vandelli Botanical Garden, " +
                   "localizado Calçada do Galvão E, 1400-171 Lisboa.\n https://www.google.com/maps/place/Vandelli+Botanical+Garden/@38.7067187,-9.2017967,17z/data=!3m1!4b1!4m6!3m5!1s0xd1ecb4279c1a8cd:0x800851f444044593!8m2!3d38.7067187!4d-9.2017967!16s%2Fg%2F11s9dk7_r6?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D \n\n" +
                   "Caso queiras dar-nos algum presente para o nosso casamento, relembramos que nos podes deixar-nos um presente através dos seguintes métodos:\n\n" +
                   "MBWAY: 918960273 / 925355133\n" +
